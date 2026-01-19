5f0d761fecf29310118121889c21c18d4217d611
raskariaka <ghost_mega@mail.ru>
2026-01-20 01:16:09 +0300

    Update shots.js

--- shots.js
+++ shots.js
@@ -1,15 +1,1 @@
-(function () {
-    'use strict';
-
-    function init() {
-        window.plugin_shots_ready = false;
-        console.log('[ShotsReady] plugin_shots_ready = false');
-    }
-
-    if (window.Lampa) {
-        init();
-    } else {
-        document.addEventListener('lampa:ready', init);
-    }
-
-})();
+alert('PLUGIN LOADED');
