f1cf0f61323a1327fe7abde56f266bebd6ee253f
raskariaka <ghost_mega@mail.ru>
2026-01-20 01:06:50 +0300

    Update shots.js

--- shots.js
+++ shots.js
@@ -1,40 +1,48 @@
 (function () {
     'use strict';
 
-    var plugin_name = 'Shots';
-    var storage_key = 'shots_ready_enabled';
+    try {
+        var plugin_name = 'Shots Ready';
+        var storage_key = 'shots_ready_enabled';
 
-    function set_flag(value) {
-        window.plugin_shots_ready = value === true;
-        console.log('[ShotsReady]', 'plugin_shots_ready =', window.plugin_shots_ready);
-    }
+        function set_flag(value) {
+            window.plugin_shots_ready = value === true;
+            console.log('[ShotsReady]', window.plugin_shots_ready);
+        }
 
-    function init() {
-        // Загружаем сохранённое значение
-        var enabled = Lampa.Storage.get(storage_key, false);
+        function init() {
+            var enabled = false;
 
-        set_flag(enabled);
-
-        // Регистрируем настройку
-        Lampa.SettingsApi.addParam({
-            component: 'extensions',
-            param: {
-                name: plugin_name,
-                type: 'toggle',
-                default: false
-            },
-            onChange: function (value) {
-                Lampa.Storage.set(storage_key, value);
-                set_flag(value);
+            if (window.Lampa && Lampa.Storage) {
+                enabled = Lampa.Storage.get(storage_key, false);
             }
-        });
-    }
 
-    // Официальное ожидание запуска LAMPA
-    if (window.Lampa) {
-        init();
-    } else {
-        document.addEventListener('lampa:ready', init);
+            set_flag(enabled);
+
+            if (window.Lampa && Lampa.SettingsApi) {
+                Lampa.SettingsApi.addParam({
+                    component: 'extensions',
+                    param: {
+                        name: plugin_name,
+                        type: 'toggle',
+                        default: false
+                    },
+                    onChange: function (value) {
+                        Lampa.Storage.set(storage_key, value);
+                        set_flag(value);
+                    }
+                });
+            }
+        }
+
+        if (window.Lampa) {
+            init();
+        } else {
+            document.addEventListener('lampa:ready', init);
+        }
+
+    } catch (e) {
+        console.error('[ShotsReady] plugin error', e);
     }
 
 })();
