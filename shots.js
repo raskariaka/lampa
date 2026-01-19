2e9824a1d1909921575f404cf09bfc79568466cc
raskariaka <ghost_mega@mail.ru>
2026-01-20 01:11:53 +0300

    Update shots.js

--- shots.js
+++ shots.js
@@ -1,31 +1,12 @@
 (function () {
     'use strict';
 
-    var plugin_name = 'Shots Ready';
-    var storage_key = 'shots_ready_enabled';
-
-    function set_flag(value) {
-        window.plugin_shots_ready = value === true;
-        console.log('[ShotsReady]', window.plugin_shots_ready);
-    }
-
     function init() {
-        var enabled = Lampa.Storage.get(storage_key, false);
-
-        set_flag(enabled);
-
-        Lampa.Settings.add({
-            title: plugin_name,
-            type: 'toggle',
-            default: false,
-            onChange: function (value) {
-                Lampa.Storage.set(storage_key, value);
-                set_flag(value);
-            }
-        });
+        window.plugin_shots_ready = false;
+        console.log('[ShotsReady] plugin_shots_ready = false');
     }
 
-    if (window.Lampa && Lampa.Settings) {
+    if (window.Lampa) {
         init();
     } else {
         document.addEventListener('lampa:ready', init);
