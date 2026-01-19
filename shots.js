b7be8ae555e6ba3683ea9832ec7aed13ed601d9a
raskariaka <ghost_mega@mail.ru>
2026-01-20 02:02:37 +0300

    Update shots.js

--- shots.js
+++ shots.js
@@ -1,15 +1,30 @@
-(function () {
+(function() {
     'use strict';
 
-    function init() {
-        window.plugin_shots_ready = true;
-        console.log('[ShotsReady] plugin_shots_ready = false');
+    function patchCard() {
+        if (!window.Lampa || !Lampa.Card) return;
+
+        // Сохраняем оригинальный метод добавления действий
+        var originalAddAction = Lampa.Card.addAction;
+
+        // Патчим метод, чтобы блокировать Shots
+        Lampa.Card.addAction = function(params) {
+            if (params && params.name === 'shots') {
+                console.log('[HideShots] blocked Shots button');
+                return; // не добавляем кнопку
+            }
+            return originalAddAction.apply(this, arguments);
+        };
+
+        console.log('[HideShots] plugin loaded');
     }
 
+    // Если Lampa уже инициализирована
     if (window.Lampa) {
-        init();
+        patchCard();
     } else {
-        document.addEventListener('lampa:ready', init);
+        // Ждём полной инициализации Lampa
+        document.addEventListener('lampa:ready', patchCard);
     }
 
 })();
