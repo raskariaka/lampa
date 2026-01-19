cb7258016d5db05792ff61eb028a1a84f46d92d2
raskariaka <ghost_mega@mail.ru>
2026-01-20 02:10:29 +0300

    Update shots.js

--- shots.js
+++ shots.js
@@ -1,19 +1,54 @@
 (function() {
     'use strict';
 
-    function hideShotsButtons() {
-        var buttons = document.querySelectorAll('.card__shots'); // класс кнопки Shots
+    var storage_key = 'hide_shots_enabled';
+    var interval_id = null;
+
+    function hideShots() {
+        var buttons = document.querySelectorAll('.card__shots');
         for (var i = 0; i < buttons.length; i++) {
             buttons[i].style.display = 'none';
         }
     }
 
-    function init() {
-        // Сразу скрываем уже существующие кнопки
-        hideShotsButtons();
+    function showShots() {
+        var buttons = document.querySelectorAll('.card__shots');
+        for (var i = 0; i < buttons.length; i++) {
+            buttons[i].style.display = '';
+        }
+    }
 
-        // Следим за новыми карточками каждые 500 мс
-        setInterval(hideShotsButtons, 500);
+    function applyFlag(enabled) {
+        if (interval_id) clearInterval(interval_id);
+
+        if (enabled) {
+            hideShots();
+            interval_id = setInterval(hideShots, 500); // отслеживаем новые карточки
+            console.log('[HideShots] Shots hidden');
+        } else {
+            showShots();
+            console.log('[HideShots] Shots visible');
+        }
+    }
+
+    function init() {
+        if (!window.Lampa || !Lampa.Settings) return;
+
+        var enabled = Lampa.Storage.get(storage_key, false);
+
+        // Добавляем пункт в меню Расширений
+        Lampa.Settings.add({
+            title: 'Удалять Shots',
+            type: 'toggle',
+            default: false,
+            onChange: function(value) {
+                Lampa.Storage.set(storage_key, value);
+                applyFlag(value);
+            }
+        });
+
+        // Применяем текущее значение
+        applyFlag(enabled);
 
         console.log('[HideShots] plugin loaded');
     }
