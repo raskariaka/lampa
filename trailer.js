5ff0437d55686733ec609532fc277f2d46173652
raskariaka <ghost_mega@mail.ru>
2026-01-20 03:49:41 +0300

    Update trailer.js

--- trailer.js
+++ trailer.js
@@ -1,37 +1,8 @@
-(function() {
-    'use strict';
-    function start() {
-        if (window.remove_trailer_button_correct) return;
-        window.remove_trailer_button_correct = true;
-        
-        // Точный селектор из DevTools
-        var correctSelector = 'div.full-start__button.selector.view--trailer';
-        
-        function removeTrailerButton() {
-            var trailerButton = document.querySelector(correctSelector);
-            if (trailerButton && trailerButton.parentNode) {
-                trailerButton.parentNode.removeChild(trailerButton);
-                console.log('[RemoveTrailer] Кнопка трейлера успешно удалена');
-                return true;
-            }
-            return false;
+(function () {
+    'use strict';	
+    Lampa.Listener.follow('full', function (e) {
+        if (e.type == 'complite') {
+            e.object.activity.render().find('.view--trailer').remove();
         }
-        
-        Lampa.Listener.follow('full', function (e) {
-            if (e.type === 'complite') {
-                // Несколько попыток с интервалами
-                setTimeout(removeTrailerButton, 100);
-                setTimeout(removeTrailerButton, 500);
-                setTimeout(removeTrailerButton, 1000);
-            }
-        });
-    }
-    
-    if (window.appready) {
-        start();
-    } else {
-        Lampa.Listener.follow('app', function (event) {
-            if (event.type === 'ready') start();
-        });
-    }
+    });
 })();
