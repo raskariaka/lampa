ab568d3566e3caa6d417b2ada745fea692d80f06
raskariaka <ghost_mega@mail.ru>
2026-01-20 03:53:29 +0300

    Update trailer.js

--- trailer.js
+++ trailer.js
@@ -1,8 +1,22 @@
-(function () {
-    'use strict';	
-    Lampa.Listener.follow('full', function (e) {
-        if (e.type == 'complite') {
-            e.object.activity.render().find('.view--trailer').remove();
-        }
-    });
+(function() {
+    'use strict';
+    function start() {
+        if (window.remove_trailer_button) return;
+        window.remove_trailer_button = true;
+        Lampa.Listener.follow('full', function (e) {
+            if (e.type === 'complite') {
+                setTimeout(function() {
+                    // Удаляем кнопку трейлеров
+                    $('.view--trailer'').remove();
+                }, 100);
+            }
+        });
+    }
+    if (window.appready) {
+        start();
+    } else {
+        Lampa.Listener.follow('app', function (event) {
+            if (event.type === 'ready') start();
+        });
+    }
 })();
