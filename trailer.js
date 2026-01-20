c17fc9fe26635aedba350a4eba2176e5b605b82f
raskariaka <ghost_mega@mail.ru>
2026-01-20 03:44:27 +0300

    Update trailer.js

--- trailer.js
+++ trailer.js
@@ -1,26 +1,32 @@
 (function() {
     'use strict';
     function start() {
-        if (window.remove_trailer_button) return;
-        window.remove_trailer_button = true;
+        if (window.remove_trailer_button_correct) return;
+        window.remove_trailer_button_correct = true;
+        
+        // Точный селектор из DevTools
+        var correctSelector = 'div.full-start__button.selector.view--trailer';
+        
+        function removeTrailerButton() {
+            var trailerButton = document.querySelector(correctSelector);
+            if (trailerButton && trailerButton.parentNode) {
+                trailerButton.parentNode.removeChild(trailerButton);
+                console.log('[RemoveTrailer] Кнопка трейлера успешно удалена');
+                return true;
+            }
+            return false;
+        }
+        
         Lampa.Listener.follow('full', function (e) {
             if (e.type === 'complite') {
-                setTimeout(function() {
-                    // Пробуем несколько возможных селекторов для трейлеров
-                    var selectors = [
-                        '.trailer-view-button',           // Основной селектор из LAMPA
-                        '[data-action="trailer"]',       // По data-атрибуту
-                        '.action-trailer',               // Альтернативный класс
-                        '.trailer-button'                // Еще один возможный вариант
-                    ];
-                    
-                    selectors.forEach(function(selector) {
-                        $(selector).remove();
-                    });
-                }, 100);
+                // Несколько попыток с интервалами
+                setTimeout(removeTrailerButton, 100);
+                setTimeout(removeTrailerButton, 500);
+                setTimeout(removeTrailerButton, 1000);
             }
         });
     }
+    
     if (window.appready) {
         start();
     } else {
